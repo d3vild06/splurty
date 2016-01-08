a614c676adbb68a3d1610b3399adc76d20f50047
Roberto Q <sgtquezada@gmail.com>
2016-01-08 05:41:44 +0000

    test updates

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,10 @@ class ActiveSupport::TestCase
   # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
+  ## for devise gem testing
+  class ActionController::TestCase
+  include Devise::TestHelpers
+  end
+
 end
